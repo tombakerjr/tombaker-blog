e46dc88e368d0b0b580acca1eed78917e65b12e0
Tom Baker <tombakerjr@outlook.com>
2024-06-14 10:59:44 -0400

    Remove obsolete code, use metadata API

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
@@ -12,23 +11,10 @@ export default function Home() {
         <p className={styles.description}>
           <a href="https://github.com/tombakerjr">Github Profile</a>
         </p>
-
-        <div className={styles.grid}>
-          
-        </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <p>&copy; 2024 Tom Baker</p>
       </footer>
     </div>
   )
